2c436de059e68daceb6a2d20a07ef139356b6e9a
andrewR23 <agrichardsoniphone@gmail.com>
2022-05-08 21:04:32 +0100

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,27 @@
+// basic colour
+// colours are values from 0 to 255
+// stroke colours
+
+function setup() {
+  createCanvas(400, 400);
+  background(60);
+  
+  fill(250, 17, 161);
+  
+  strokeWeight (2);
+  stroke (255);
+  line(100, 200, 100, 50);
+
+  stroke (66, 192, 255);
+  strokeWeight (15);
+  line(200, 200, 200, 50);
+  
+  stroke(250, 7, 214);
+  strokeWeight (40);
+  line(300, 200, 300, 50);
+  
+  
+
+}
+
+function draw() {}
